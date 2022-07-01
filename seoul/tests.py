0ab5b77e6e856ca939bcd495312e8cbd3029d659
KIWONY <kien1004@naver.com>
2022-07-01 14:21:27 +0900

    #5-test-우량계 데이터 확인 테스트

--- seoul/tests.py
+++ seoul/tests.py
@@ -1,5 +1,6 @@
 from django.test import RequestFactory, TestCase
-from seoul.views import SeoulWaterLevelRainFallView, dict_gu_id
+from seoul.views import SeoulWaterLevelRainFallView, dict_gu_id ,OPEN_API_KEY
+import json , requests
 
 
 class SeoulTest(TestCase):
@@ -40,3 +41,45 @@ class SeoulTest(TestCase):
 			self.assertIsNotNone(raingauge_info)
 			self.assertIsNotNone(rainguage_name)
 			self.assertIsNotNone(sum_rain_fall)
+
+	'''
+    date : 2022-06-30
+    writer : 전기원
+    '''
+
+	# view api의 우량계 리스트와 open api의 우량계 리스트가 동일한지 테스트하는 코드입니다.
+	def test_get_gauge(self):
+		gu_name = '종로구'
+
+		#view api의 우량계 리스트를 가져오는 코드입니다.
+		url = 'http://127.0.0.1:8000/api/seoul/gu-search/'
+		factory = RequestFactory()
+		view = SeoulWaterLevelRainFallView.as_view()
+		request = factory.get(url, format = 'json')
+		response = view(request, gu_name)
+		self.assertEqual(response.status_code, 200)
+		
+		gauge_list = []
+		for info in response.data['data']['raingauge_info']:
+      
+			# 데이터가 없다면 detail이 나옴. detail이 없을 경우, continue합니다.
+			if 'detail' in response.data: 
+				continue
+			gauge_list.append(info['raingauge_name'])
+
+
+		# open api에서 우량계 리스트를 가져오는 코드 입니다.
+		rainfall_URL = f'http://openapi.seoul.go.kr:8088/{OPEN_API_KEY}/json/ListRainfallService/1/100/{gu_name}'
+		rainfall_response = json.loads(requests.get(rainfall_URL).content)
+		
+		open_gauge_list = set()
+		for info in rainfall_response['ListRainfallService']['row']:
+			open_gauge_list.add(info['RAINGAUGE_NAME'])
+		open_gauge_list = list(open_gauge_list)
+				
+		# 두 리스트의 요소들을 정렬합니다.
+		gauge_list.sort()
+		open_gauge_list.sort()
+				
+		# 유닛테스트에서 두 리스트의 값이 동일한지 확인합니다.
+		self.assertEqual(open_gauge_list, gauge_list)
